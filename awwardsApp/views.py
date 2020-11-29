da263f1a723d1fb71ae7a9a6fb08d381fee07c99
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-29 15:56:37 +0300

    Add imports.

--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404, HttpResponseRedirect
+import datetime as dt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.models import User
+from django.urls import reverse, reverse_lazy
+from django.views.generic import DetailView, FormView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import *
+from .forms import *
+
 
 # Create your views here.
